e1a4f7912564732af6890dc9d58a6c6904b025fc
magicprem <magicprem@gmail.com>
2023-08-18 13:28:46 +0530

    new

---
+++ src/com/javaclass/Employee.java
@@ -0,0 +1,18 @@
+package com.javaclass;
+
+public class Employee {
+	
+	public void Company  {
+		System.out.println("Qa services");
+		
+}
+	
+	public static void main(String[] args) {
+		
+		Employee obj = new Employee();
+		obj.Company();
+		
+		
+	}
+
+}
---
+++ src/com/javaclass/Owner.java
@@ -0,0 +1,11 @@
+package com.javaclass;
+
+public class Owner {
+
+	public void sandy() {
+		
+		System.out.println("This is demo");
+
+	}
+	
+}
